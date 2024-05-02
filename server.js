6e67fbf0db906d24e8955aefbe2795264ba4ecf2
diwanumang <diwan.umang@gmail.com>
2024-05-01 20:22:47 -0400

    Initial Commit/Completed create and read part of CRUD

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+const methodOverride = require('method-override');
+const path = require('path');
+const Player = require('./model/playerSchema.js');
+
+const app = express();
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, "public")));
+
+mongoose.connect(process.env.MONGODB_URI);
+
+app.listen(3000, () => {
+    console.log('Working fine!');
+})
+
+app.get('/', (req, res) => {
+    res.render('homePage.ejs');
+})
+
+app.get('/roster', async (req, res) => {
+    const fullRoster = await Player.find();
+    res.render('roster.ejs', {
+        fullRoster: fullRoster,
+    });
+})
+
+app.get('/roster/newPlayer', (req, res) => {
+    res.render('newPlayer.ejs');
+})
+
+app.post('/roster', async (req, res) => {
+    await Player.create(req.body);
+    res.redirect('/roster');
+})
+
+app.get('/roster/:playerId', async (req, res) => {
+    const selectedPlayer = await Player.findById(req.params.playerId);
+    res.render('show.ejs', {
+        selPlayer: selectedPlayer,
+    });
+})
